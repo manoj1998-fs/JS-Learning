9b263da548f14a42d622b010c115ad7e935af190
Manoj Kumar Mishra <manoj.mishra@nirmalyalabs.com>
2026-04-29 16:00:18 +0530

    basic

---
+++ basics_01/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 267236782;
+let accountMail = "mail4msihra@gmail.com";
+var accountPass = "2384728";
+accoutCity = "Odisha";
+/*
+Prefered not to use var because its gives the issue of block scope and functional scope
+*/
+console.table([accountId,accountMail,accountPass]);
