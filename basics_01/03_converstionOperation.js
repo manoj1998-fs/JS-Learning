e86029aa074c3cb0d9095dc8f97306e8a3e95dbf
Manoj Kumar Mishra <manoj.mishra@nirmalyalabs.com>
2026-05-06 15:08:45 +0530

    06 second hour

---
+++ basics_01/03_converstionOperation.js
@@ -0,0 +1,13 @@
+let score = "30";
+console.log(typeof score);
+let convertedValue = Number(score);
+console.log(typeof convertedValue);
+console.log(convertedValue);
+
+// "333abv" => NaN
+// true => 1, false => 0
+//Null => 0:
+//"" => false and "Manoj"=> true
+let demoString = "";
+let booleanConvert = Boolean(demoString);
+console.log(booleanConvert)
