904f6de68191fdce4ce60a5143e27a0cab1befe4
Manoj Kumar Mishra <manoj.mishra@nirmalyalabs.com>
2026-05-06 10:17:43 +0530

    may 6

---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,13 @@
+let name = "manoj"; //String
+let age = 3; //integer
+let isverified = true;// boolean
+
+// number => 2 to the power 56
+//bigint
+//string =>""
+//boolean => true / false ;
+// null => stand alone value ;
+// undefined => state
+// symbol => unique;
+// object
+console.log(typeof(null)) ; // Object 
