288018390822a21a85cab101bfb23a4b46c0274b
Manoj Kumar Mishra <manoj.mishra@nirmalyalabs.com>
2026-05-21 14:22:16 +0530

    string

---
+++ basics_01/04_string.js
@@ -0,0 +1,20 @@
+// const newString = "test123";
+// console.log(newString.match(/\d+/));
+// console.log(typeof newString.match(/\d+/));
+
+// const demoString = " Hello World   ";
+// console.log(demoString.trim());
+
+// let  str1 = "ab";
+// console.log(str1.repeat(3));
+
+// console.log("5".padStart(4,'0') );   //0005
+// console.log("5".padEnd(4,'0') )  //5000
+
+// let stringlower = "male ego";
+// console.log(stringlower.toUpperCase());
+// let url = "https://manojkumar7349%20mishra.com"
+// console.log(url.replace("%20",'-'));
+// console.log(url.includes("kumar"));
+let stringToSplit = "Here Is The Demo ";
+console.log(stringToSplit.split(''));
